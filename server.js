c9589c240a1a60bb41b1feba5f657772fef46576
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2023-07-14 08:21:16 -0400

    load project assets to server

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 //import dependencies
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser')
 
 
 
 //create instance of express app
 const app = express();
 
+//log every request
+app.use(morgan('tiny'))
+
+//parse every request through body-parser
+app.use(bodyparser.urlencoded({extended:true}))
+
+//set view engine
+app.set('view engione', 'ejs')
+
+//if ejs files were in a different folder
+// app.set('views', path.resolve(__dirname, "views/ejs"))
+
+
+
+//load assets to server
+app.use('/css', express.static(path.resolve(__dirname, "asset/css")))
+app.use('/img', express.static(path.resolve(__dirname, "asset/img")))
+app.use('/js', express.static(path.resolve(__dirname, "asset/js")))
 
 //default root route 
 app.get('/', (req, res) => {
